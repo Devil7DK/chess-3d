870b61bce1748053eedae63f7859f02c3de35cbc
Devil7DK <dineshthangavel47@gmail.com>
2023-05-11 00:11:00 +0530

    Add dropdown to change environment

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -1,32 +1,60 @@
 import { OrbitControls, Stage } from '@react-three/drei';
+import { PresetsType } from '@react-three/drei/helpers/environment-assets';
 import { Canvas } from '@react-three/fiber';
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 
 import { Board } from './components';
 
 export const Scene: React.FC = () => {
-    return (
-        <Canvas
-            shadows
-            dpr={[1, 2]}
-            camera={{
-                position: [0, 0, 150],
-                fov: 40,
-            }}
-        >
-            <Suspense>
-                <Stage environment='city' intensity={0.6}>
-                    <Board
-                        borderColor='grey'
-                        borderWidth={2}
-                        cellSize={5}
-                        position={{ x: 0, y: -0.5, z: 0 }}
-                        thickness={1}
-                    />
-                </Stage>
-            </Suspense>
+    const [environment, setEnvironment] = useState<PresetsType>(
+        (localStorage.getItem('environment') as PresetsType) || 'sunset'
+    );
 
-            <OrbitControls />
-        </Canvas>
+    return (
+        <>
+            <div className='overlay'>
+                <select
+                    value={environment}
+                    onChange={(e) => {
+                        setEnvironment(e.target.value as PresetsType);
+                        localStorage.setItem('environment', e.target.value);
+                    }}
+                >
+                    <option value='sunset'>sunset</option>
+                    <option value='dawn'>dawn</option>
+                    <option value='night'>night</option>
+                    <option value='warehouse'>warehouse</option>
+                    <option value='forest'>forest</option>
+                    <option value='apartment'>apartment</option>
+                    <option value='studio'>studio</option>
+                    <option value='city'>city</option>
+                    <option value='park'>park</option>
+                    <option value='lobby'>lobby</option>
+                </select>
+            </div>
+            <Canvas
+                shadows
+                dpr={[1, 2]}
+                camera={{
+                    position: [0, 0, 150],
+                    fov: 40,
+                }}
+            >
+                <color attach='background' args={['#202030']} />
+                <Suspense>
+                    <Stage environment={environment} intensity={0.6}>
+                        <Board
+                            borderColor='grey'
+                            borderWidth={2}
+                            cellSize={5}
+                            position={{ x: 0, y: -0.5, z: 0 }}
+                            thickness={1}
+                        />
+                    </Stage>
+                </Suspense>
+
+                <OrbitControls />
+            </Canvas>
+        </>
     );
 };
