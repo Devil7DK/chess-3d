2c78fa2a8e79e868f0cbd43949d1e9bc1a4bee98
Devil7DK <dineshthangavel47@gmail.com>
2023-05-10 14:21:08 +0530

    Add support for importing JPG & PNG

--- custom.d.ts
+++ custom.d.ts
@@ -2,3 +2,13 @@ declare module '*.gltf' {
     const value: string;
     export default value;
 }
+
+declare module '*.jpg' {
+    const value: string;
+    export default value;
+}
+
+declare module '*.png' {
+    const value: string;
+    export default value;
+}
